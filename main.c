09ab9dfe38075cc2ab05f1ab903e245e22b17829
Yulin <zhengyulin@gmail.com>
2020-11-03 00:59:05 -0500

    finished assignment

--- main.c
+++ main.c
@@ -5,16 +5,39 @@
 #include "power_level.h"
 
 int main() {	
-	struct z_fighter *Goku = create_z("Goku", "Saiyan", 9000, "Yellow");
-	struct z_fighter *Krillin = create_z("Krillin", "Human", 10, "Black");
-	struct z_fighter *Piccolo = create_z("Piccolo", "Namekian", 5000, "Bald");
-	struct z_fighter *Cell = create_z("Cell", "Bio-Android", 200000, "Green");
-	Goku->next = Krillin;
-	Krillin->next = Piccolo;
-	Piccolo->next = Cell;
-	struct z_fighter *Gohan = insert_front(Goku, "Gohan", "Half-Saiyan", 10000, "Black");
-	remove_node(Gohan, "Goku");
-	print_list(Gohan);
+	struct z_fighter *Z_Fighters = NULL;
+	Z_Fighters = insert_front(Z_Fighters, "Goku", "Saiyan", 9000, "Yellow");
+	Z_Fighters = insert_front(Z_Fighters, "Vegeta", "Saiyan", 200000, "Green");
+	Z_Fighters = insert_front(Z_Fighters, "Krillin", "Human", 10, "Black");
+	Z_Fighters = insert_front(Z_Fighters, "Piccolo", "Namekian", 5000, "Bald");
+	Z_Fighters = insert_front(Z_Fighters, "Cell", "Bio-Android", 200000, "Green");
+	Z_Fighters = insert_front(Z_Fighters, "Gohan", "Half-Saiyan", 10000, "Black");
+	printf("\nBehold, the Z-Fighters!");
+	print_list(Z_Fighters);
+	printf("\n");
+
+	printf("Removing Krillin:");
+	Z_Fighters = remove_node(Z_Fighters, "Krillin");
+	print_list(Z_Fighters);
+	printf("\n");
+
+	printf("Removing Gohan:");
+	Z_Fighters = remove_node(Z_Fighters, "Gohan");
+	print_list(Z_Fighters);
+	printf("\n");
+
+	printf("Removing Goku:");
+	Z_Fighters = remove_node(Z_Fighters, "Goku");
+	print_list(Z_Fighters);
+	printf("\n");
+
+	printf("Removing Android 17 (Not on the list, so nothing should change):");
+	Z_Fighters = remove_node(Z_Fighters, "Android 17");
+	print_list(Z_Fighters);
+	printf("\n");
+
+	printf("Freeing Everyone Else:\n");
+	free_list(Z_Fighters);
 
 	return 0;
 }
--- power_level.c
+++ power_level.c
@@ -2,33 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "power_level.h"
-struct z_fighter * create_z(char *n, char *s, int p_lvl, char *h) {
-
-  struct z_fighter *fighter;
-  fighter = malloc(sizeof(struct z_fighter));
-  strncpy(fighter->name, n, sizeof(fighter->name)-1);
-  strncpy(fighter->species, s, sizeof(fighter->species)-1);
-  strncpy(fighter->hair_color, h, sizeof(fighter->hair_color)-1);
-  fighter->power_lvl = p_lvl;
-
-  return fighter;
-}
-
-void print_z (struct z_fighter *f) {
-	printf("Name: %s\nSpecies: %s\nPower Level: %d\nHair Color: %s\n", f->name, f->species, f->power_lvl, f->hair_color);
-}
-
-void change_z (struct z_fighter *z_f, int p_lvl, char *h) {
-  z_f->power_lvl = p_lvl;
-  strncpy(z_f->hair_color, h, sizeof(z_f->hair_color)-1);
-}
 
 void print_list(struct z_fighter *f) {
   struct z_fighter *z = f;
   printf("\n");
   while (f) {
     print_z(f);
-    printf("\n");
     f = f->next;
   }
 }
@@ -48,6 +27,7 @@ struct z_fighter * free_list(struct z_fighter *head) {
     //ensures that the next node is pointing to null
     //t should still point to the next null
     f->next = NULL;
+    printf("Freeing %s\n", f->name);
     free(f);
     f = t;
   }
@@ -61,23 +41,37 @@ struct z_fighter * remove_node(struct z_fighter *head, char *name) {
   if (!strcmp(fighter->name, name)) {
     fighter = head->next;
     free(head);
+    //not returning head node here because that's what we're removing
     return fighter;
   }
-  while (fighter) {
+  while (fighter->next) {
     struct z_fighter *next_fighter = fighter->next;
-    if (!strcmp(fighter->next->name, name)) {
-      next_fighter = fighter->next;
-      fighter->next = fighter->next->next;
+    if (!strcmp(next_fighter->name, name)) {
+      fighter->next = next_fighter->next;
       free(next_fighter);
     }
+    //checks if this is the last node
     if (fighter->next) {
-      printf("s1s\n");
       fighter = fighter->next;
-      printf("s2\n");
     }
-    
-    printf("s3s\n");
   }
-  printf("sdas\n");
+  return head;
+}
+
+
+struct z_fighter * create_z(char *n, char *s, int p_lvl, char *h) {
+
+  struct z_fighter *fighter;
+  fighter = malloc(sizeof(struct z_fighter));
+  strncpy(fighter->name, n, sizeof(fighter->name)-1);
+  strncpy(fighter->species, s, sizeof(fighter->species)-1);
+  strncpy(fighter->hair_color, h, sizeof(fighter->hair_color)-1);
+  fighter->power_lvl = p_lvl;
+
   return fighter;
 }
+
+void print_z (struct z_fighter *f) {
+  printf("Name: %s, Species: %s, Power Level: %d, Hair Color: %s \n", f->name, f->species, f->power_lvl, f->hair_color);
+}
+
